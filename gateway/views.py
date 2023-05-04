505abc4ba1b8d49d9ee8d61df8305638f5d847d9
Timmi <danieloluwoleoluwatimileyin@gmail.com>
2023-05-04 13:43:37 +0100

    Jwt token authentication

--- gateway/views.py
+++ gateway/views.py
@@ -1,3 +1,44 @@
+import jwt
 from django.shortcuts import render
+from .models import Jwt
+from datetime import datetime, timedelta
+from django.conf import settings
+import random, string
+from rest_framework.views import APIView
+from .serializers import LoginSerializer
+from django.contrib.auth import authenticate
+from rest_framework.response import Response
 
 # Create your views here.
+def get_random(length):
+    ''.join(random.choices(string.ascii_uppercase + string.digits, k=length))
+def get_access_token(payload):
+    return jwt.encode(
+        {"exp": datetime.now() + timedelta(minutes=5), **payload},
+        settings.SECRET_KEY,
+        algorithm="HS256"
+    )
+
+def get_refresh_token():
+    return jwt.encode(
+        {"exp": datetime.now() + timedelta(days=365), "data": get_random(10)},
+        settings.SECRET_KEY,
+        algorithm="HS256"
+    )
+
+class Loginview(APIView):
+    serializer_class = LoginSerializer
+    def post(self, request):
+        serializer = self.serializer_class
+        serializer.is_valid(raise_exception=True)
+
+        user = authenticate(username=serializer.validated_data['email'], password=serializer.validated_data['password'])
+        if not user:
+            return Response({"error": "Invalid email or password"}, status="400")
+
+        access = get_access_token({"user_id":user.id})
+        refresh = get_refresh_token()
+
+        Jwt.objects.create(user = user.id, access_token = access, refresh_token= refresh)
+
+        return Response({"access_token":access, "refresh_token": refresh})
