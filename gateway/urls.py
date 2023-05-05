61e69e1068edb0596d14e90c1a39f7cf55f487bb
Timmi <danieloluwoleoluwatimileyin@gmail.com>
2023-05-05 22:53:58 +0100

    commit

--- gateway/urls.py
+++ gateway/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('login/', Loginview.as_view()),
     path('register/', Registerview.as_view()),
     path('refresh/', Refreshview.as_view()),
-    path('securedinfo', Getsecuredinfo.as_view())
+    path('securedinfo/', Getsecuredinfo.as_view()),
 ]
