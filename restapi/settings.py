39d781841b40476202bdb09b778f25accb12edbc
Timmi <danieloluwoleoluwatimileyin@gmail.com>
2023-05-07 17:57:27 +0100

    DJ_SECRET_KEy

--- restapi/settings.py
+++ restapi/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qe7r&b)^ha+5vd0wiau7w(!uju0cj+mzpp24i6clgutys3gz-7'
+SECRET_KEY = config("DJ_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
