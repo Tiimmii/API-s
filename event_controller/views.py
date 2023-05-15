eb1f1ad4127d57e637336a77bd4e79d6d02a56d3
Timmi <danieloluwoleoluwatimileyin@gmail.com>
2023-05-15 10:55:29 +0100

    viewss

--- event_controller/views.py
+++ event_controller/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .models import EventMain, EventAttender, EventFeature
+from .serializer import EventmainSerializer
+from rest_framework.viewsets import ModelViewSet
 # Create your views here.
+class Eventview(ModelViewSet):
+    serializer_class = EventMain
+    queryset = EventMain.objects.select_related("user_event_author", "event_address")
