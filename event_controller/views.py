569a69a08380b8579520a041bae98e23d66f4dea
Timmi <danieloluwoleoluwatimileyin@gmail.com>
2023-05-16 08:40:48 +0100

    create event

--- event_controller/views.py
+++ event_controller/views.py
@@ -3,6 +3,7 @@ from .models import EventMain, EventAttender, EventFeature
 from .serializer import EventmainSerializer, EventFeatureSerializer
 from rest_framework.viewsets import ModelViewSet
 from customuser.serializer import AddressSerailizer
+from customuser.models import Address_global
 # Create your views here.
 class Eventview(ModelViewSet):
     serializer_class = EventmainSerializer
@@ -14,3 +15,13 @@ class Eventview(ModelViewSet):
         a_serializer.save()
 
         data = {**request.data, "address_info_id":a_serializer["id"]}
+        e_serializer = self.serializer_class(data=data)
+        if not e_serializer.is_valid():
+            Address_global.objects.filter(id=e_serializer["address_info_id"]).delete()
+            raise Exception(e_serializer.errors)
+        e_serializer.save()
+
+        features = request.data.get("features", None)
+        if not isinstance(list, features):
+            features = [features]
+        
