2de70ca3ec29c76aef6f81feba33a3074e80564a
Timmi <danieloluwoleoluwatimileyin@gmail.com>
2023-05-16 08:35:24 +0100

    create

--- event_controller/serializer.py
+++ event_controller/serializer.py
@@ -1,11 +1,18 @@
 from rest_framework import serializers
 from customuser.serializer import CustomuserSerializer, AddressSerailizer
 from .models import EventMain, EventFeature, EventAttender
+
+class EventFeatureSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = EventFeature
+        fields = '__all__'
+
 class EventmainSerializer(serializers.ModelSerializer):
     author = CustomuserSerializer(read_only=True)
     author_id = serializers.IntegerField(write_only = True)
     address_info = AddressSerailizer(read_only = True)
     address_info_id = serializers.IntegerField(write_only = True)
+    event_feature = EventFeatureSerializer(read_only=True, many=True)
 
     class Meta:
         model = EventMain
--- event_controller/views.py
+++ event_controller/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from .models import EventMain, EventAttender, EventFeature
-from .serializer import EventmainSerializer
+from .serializer import EventmainSerializer, EventFeatureSerializer
 from rest_framework.viewsets import ModelViewSet
+from customuser.serializer import AddressSerailizer
 # Create your views here.
 class Eventview(ModelViewSet):
     serializer_class = EventmainSerializer
-    queryset = EventMain.objects.select_related("author", "address_info")
+    queryset = EventMain.objects.select_related("author", "address_info").prefetch_related("event_feature")
+
+    def create(self, request, *args, **kwargs):
+        a_serializer = AddressSerailizer(data=request.data)
+        a_serializer.is_valid(raise_exception=True)
+        a_serializer.save()
+
+        data = {**request.data, "address_info_id":a_serializer["id"]}
